d266a5258ea49ce5878110f72c78305f4d2ed3bd
swat08mx <70145860+swat08mx@users.noreply.github.com>
2022-07-13 00:06:11 +0530

    Update swat.py
    added 5 numbers support and  fixed some bugs

--- swat.py
+++ swat.py
@@ -1,5 +1,5 @@
 import math
-print(" Available functions are- + - * / ^  log (V1,V2,S1,S2) ")
+print(" Available functions are- + - * / ^  log (V1,V2,S1,S2) root ")
 sign = input("Enter a sign: ")
 
 if sign == "V1":
@@ -23,7 +23,9 @@ elif sign == "S2":
        V1 = input("Enter the value of V1: ")
        print((float(V1) * float(S1)) / float(V2))
 
-elif sign == "^":
+
+
+if sign == "^":
     num1 = input("Enter a number: ")
     power = input("Enter a power: ")
     print(int(num1) ** int(power))
@@ -32,27 +34,120 @@ elif sign == "log":
     num1 = input("Enter a number: ")
     print(math.log(int(num1)))
 
-
-elif sign == "+":
-    num1 = input("Enter a number: ")
-    num2 = input("Enter another number: ")
-    print(float(num1) + float(num2))
-
-elif sign == "-":
-    num1 = input("Enter a number: ")
-    num2 = input("Enter another number: ")
-    print(float(num1) - float(num2))
-elif sign == "*":
-    num1 = input("Enter a number: ")
-    num2 = input("Enter another number: ")
-    print(float(num1) * float(num2))
 elif sign == "/":
     num1 = input("Enter a number: ")
     num2 = input("Enter another number: ")
     print(float(num1) / float(num2))
+elif sign == "root":
+    num1 = input("Enter a number to be sq rooted: ")
+    print(math.sqrt(float(num1)))
+    quit()
+    
 
-else:
+def twonum():
+   if sign == "+":
+    num1 = input("Enter a number: ")
+    num2 = input("Enter another number: ")
+    print(float(num1) + float(num2))
+
+   elif sign == "-":
+    num1 = input("Enter a number: ")
+    num2 = input("Enter another number: ")
+    print(abs(num1) - abs(num2))
+   elif sign == "*":
+    num1 = input("Enter a number: ")
+    num2 = input("Enter another number: ")
+    print(float(num1) * float(num2))
+  
+
+   else:
     print("Enter a valid operator")
+
+def threenum():
+    if sign == "+":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     print(float(num1) + float(num2) + float(num3))
+
+    elif sign == "-":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     print(abs(num1) - abs(num2) - abs(num3))
+    elif sign == "*":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     print(float(num1) * float(num2) * float(num3))
+    
+    else:
+     print("Enter a valid operator")
+
+def fournum():
+    if sign == "+":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     print(float(num1) + float(num2) + float(num3) + float(num4))
+
+    elif sign == "-":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     print(abs(num1) - abs(num2) - abs(num3) - abs(num4))
+    elif sign == "*":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     print(float(num1) * float(num2) * float(num3) * float(num4))
+    
+    else:
+     print("Enter a valid operator")
+
+def fivenum():
+    if sign == "+":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     num5 = input("Enter 5th number: ")
+     print(float(num1) + float(num2) + float(num3) + float(num4) + float(num5))
+
+    elif sign == "-":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     num5 = input("Enter 5th number: ")
+     print(abs(num1) - abs(num2) - abs(num3) - abs(num4) - abs(num5))
+
+    elif sign == "*":
+     num1 = input("Enter a number: ")
+     num2 = input("Enter another number: ")
+     num3 = input("Enter 3rd number: ")
+     num4 = input("Enter 4th number: ")
+     num5 = input("Enter 5th number: ")
+     print(float(num1) * float(num2) * float(num3) * float(num4) * float(num5))
+    
+    else:
+     print("Enter a valid operator")
+
+
+
+
+numbers = input("Enter number of numbers: ")
+if numbers == "2":
+    twonum()
+elif numbers == "3":
+    threenum()
+elif numbers == "4":
+    fournum()
+elif numbers == "5":
+    fivenum()
 
 
 
