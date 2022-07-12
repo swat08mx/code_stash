908ea69d53a3f753daac27032957a71d3f9f63e6
swat08mx <70145860+swat08mx@users.noreply.github.com>
2022-07-12 13:24:31 +0530

    Update swat.py
    added V1S1=V2S2 formula

--- swat.py
+++ swat.py
@@ -1,6 +1,28 @@
 import math
-print(" Available functions are- + - * / ^  log")
+print(" Available functions are- + - * / ^  log (V1,V2,S1,S2) ")
 sign = input("Enter a sign: ")
+
+if sign == "V1":
+       S1 = input("Enter the value of S1: ")
+       V2 = input("Enter the value of V2: ")
+       S2 = input("Enter the value of S2: ")
+       print((float(V2) * float(S2)) / float(S1))
+elif sign == "V2":
+       V1 = input("Enter the value of V1: ")
+       S1 = input("Enter the value of S1: ")
+       S2 = input("Enter the value of S2: ")
+       print((float(V1) * float(S1)) / float(S2))
+elif sign == "S1":
+       V1 = input("Enter the value of V1: ")
+       V2 = input("Enter the value of V2: ")
+       S2 = input("Enter the value of S2: ")
+       print((float(V2) * float(S2)) / float(V1))
+elif sign == "S2":
+       S1 = input("Enter the value of S1: ")
+       V2 = input("Enter the value of V2: ")
+       V1 = input("Enter the value of V1: ")
+       print((float(V1) * float(S1)) / float(V2))
+
 num1 = input("Enter a number: ")
 
 if sign == "^":
@@ -14,6 +36,7 @@ elif sign == "log":
 elif sign == "+":
        num2 = input("Enter another number: ")
        print(float(num1) + float(num2))
+
 elif sign == "-":
        num2 = input("Enter another number: ")
        print(float(num1) - float(num2))
