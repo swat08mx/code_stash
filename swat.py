34df7c668822f561d29bf4a573b8af9a6657ab85
swat08mx <70145860+swat08mx@users.noreply.github.com>
2022-07-12 12:32:02 +0530

    Update swat.py

--- swat.py
+++ swat.py
@@ -1,20 +1,32 @@
-print(" Available functions are- + - * / ^ ")
+import math
+print(" Available functions are- + - * / ^  log")
 sign = input("Enter a sign: ")
 num1 = input("Enter a number: ")
+
 if sign == "^":
     power = input("Enter a power: ")
     print(int(num1) ** int(power))
-else:
-    num2 = input("Enter another number: ")
 
-if sign == "+":
-    print(float(num1) + float(num2))
+elif sign == "log":
+       print(math.log(int(num1)))
+
+
+elif sign == "+":
+       num2 = input("Enter another number: ")
+       print(float(num1) + float(num2))
 elif sign == "-":
-    print(float(num1) - float(num2))
+       num2 = input("Enter another number: ")
+       print(float(num1) - float(num2))
 elif sign == "*":
-    print(float(num1) * float(num2))
+       num2 = input("Enter another number: ")
+       print(float(num1) * float(num2))
 elif sign == "/":
-    print(float(num1) / float(num2))
+       num2 = input("Enter another number: ")
+       print(float(num1) / float(num2))
+
+else:
+    print("Enter a valid operator")
+
 
 
 
